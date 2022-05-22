b23247e4310951f925cdd29537e5fca29c8d8239
yunh <yunh04131@gmail.com>
2022-05-22 22:37:42 +0900

    [FRONT] ADD 영화 리스트 View 완료

--- final-pjt-front/src/views/MovieListView.vue
+++ final-pjt-front/src/views/MovieListView.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
     <h3>MovieListView</h3>
-    <MovieListItem/>
+    <ul>
+      <li>
+        <button>유저들이 가장 많이 보고 싶어하는 영화</button>
+        </li>
+      <li><button>최신 영화</button></li>
+      <li><button>사용자 맞춤 추천 영화</button></li>
+    </ul>
+    <ul>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <MovieListItem v-for="movie in movies" :key="movie.pk" :movie="movie"/>
+      </div>
+    </ul>
   </div>
 </template>
 
 <script>
   import MovieListItem from '@/components/MovieListItem.vue'
   import { mapActions, mapGetters } from 'vuex'
-  
+
   export default {
     name: 'MovieListView',
     components: { MovieListItem },
+    computed: {
+      ...mapGetters(['movies'])
+    },
+    methods: {
+      ...mapActions(['fetchMovies'])
+    },
+    created() {
+      this.fetchMovies('recent')
+    }
   }
 </script>
 
