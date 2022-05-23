d30df64dac6faa5b69f52d51a52d109a6fac3f7f
yunh <yunh04131@gmail.com>
2022-05-24 02:33:14 +0900

    [FRONT] Article 리스트 보기, Detail 모달로 보기 구현

--- final-pjt-front/src/components/ArticleForm.vue
+++ final-pjt-front/src/components/ArticleForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h3>ArticleCreateModal</h3>
+    <h3>ArticleForm</h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ArticleCreateModal',
+  name: 'ArticleForm',
 }
 </script>
 
--- final-pjt-front/src/components/ArticleListItem.vue
+++ final-pjt-front/src/components/ArticleListItem.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    <h3>ArticleListItem</h3>
-    <ArticleDetailModal/>
-  </div>
+  <tr>
+    <th scope="row" @click="open">{{ index }}</th>
+    <td @click="open">{{ article_info.title }}</td>
+    <td @click="open">{{ article_info.user.username }}</td>
+    <td @click="open">{{ article_info.comment_count }} | {{ article_info.like_count }}</td>
+    <modal name='showArticle' height="auto" width="50%">
+      <h3>{{ article.title }}</h3>
+      <p>{{ article.content }}</p>
+    </modal>
+  </tr>
 </template>
 
 <script>
-  import ArticleDetailModal from './ArticleDetailModal.vue'
+  import Vue from 'vue'
+  import { mapGetters, mapActions } from 'vuex'
+  import VModal from 'vue-js-modal'
+  import 'vue-js-modal/dist/styles.css'
+
+  Vue.use(VModal)
+
   export default {
     name: 'ArticleListItem',
-    components: { ArticleDetailModal },
+    props: {
+      index: Number,
+      article_info: Object,
+    },
+    computed: {
+      ...mapGetters(['article'])
+    },
+    methods: {
+      ...mapActions(['fetchArticle']),
+      open () {
+        this.fetchArticle(this.article_info.pk)
+        this.$modal.show('showArticle')
+      }
+    }
   }
 </script>
 
