ea4f4895949b9a560c4db70ad020cec0c4ad0b10
mooyah5 <qorgkssk123@gmail.com>
2022-05-26 00:49:32 +0900

    [FRONT] UPDATE haha

--- final-pjt-front/src/components/ArticleDetail.vue
+++ final-pjt-front/src/components/ArticleDetail.vue
@@ -1,34 +1,52 @@
 <template>
-  <div class="modal-body">
-    <div>
-      <div class='d-flex justify-content-between'>
-        <div>
-          <h3>{{ article.title }}</h3>
-          <p>content: {{ article.content }}</p>
+  <div class="modal-body px-5 py-3">
+    <div class="container">
+      <div class="row text-center py-5">
+        <h3>{{ article.title }}</h3>
+      </div>
+
+      <div class="row justify-content-between py-2">
+        <div class="col-2">
+          
         </div>
-        <div v-if='article.user'>
-          <div>작성자 : {{ article.user.username }}</div>
-          <h4>
-            <button v-if="isLoggedIn" @click='likeArticle(article.pk)'>
-              <i v-if="article.is_liked" class="fa-solid fa-heart"></i>
+        <div class="col-2">
+          작성자 : {{ article.user.username }}
+        </div>
+      </div>
+      <div class="row text-center py-2">
+        <p>{{ article.content }}</p> 
+      </div>
+      <div v-if='article.user' class="row py-5 ">
+           <h4>
+               <a v-if="isLoggedIn" @click='likeArticle(article.pk)'>
+              <i v-if="article.is_liked" class="fa-solid fa-heart text-danger"></i>
               <i v-else class="fa-regular fa-heart"></i>
-            </button>
+            </a>
             <i v-else class="fa-solid fa-heart"></i>
             {{ article.like_users.length }}<br>
-            <button @click="open" class='btn btn-warning'>수정</button>
-            <button @click="deleteArticle(article.pk)" class="btn btn-danger">삭제</button>
-          </h4>
-        </div>
+           </h4>
       </div>
+      <div class='row pb-5 d-flex justify-content-end'>
+        <div class="col-1"> </div>
+        <button class="btn btn-warning col-2" @click="open" >수정</button>
+        <div class="col-1"> </div>
+        <button class="btn btn-danger col-2" @click="deleteArticle(article.pk)" >삭제</button>
+        
+      </div>
+      <hr>
+      <br><br><br>
+      
+      <h4>댓글 목록</h4>
       <div>
         <CommentList :comments='article.comments'/>
-        <hr>
+
         <CommentForm :articlePk='article.pk'/>
       </div>
-    </div>
+ 
     <modal name='updateModal' height="auto" width="50%">
       <ArticleForm :article='article' action="update"/>
     </modal>
+    </div>
   </div>
 </template>
 
@@ -41,9 +59,7 @@ import CommentForm from './CommentForm.vue'
 import Vue from 'vue'
 import VModal from 'vue-js-modal'
 import 'vue-js-modal/dist/styles.css'
-
 Vue.use(VModal)
-
 export default {
   name: 'ArticleDetail',
   components: { ArticleForm, CommentList, CommentForm },
@@ -64,5 +80,4 @@ export default {
       height: 100%;
       overflow-y: auto;
   }
-
 </style>
